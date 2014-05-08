febad85e992ace31fdd30b850d72f9d605c66de3
jerson betin <jersonbetin@hotmail.com>
2014-05-08 10:22:51 -0500

    se agrego un filtro de tipo de profesional por su codigo

--- routes/api/tipoProfesional.js
+++ routes/api/tipoProfesional.js
@@ -11,3 +11,13 @@ exports.getProfesionales = function (req, res){
     }
   });
 }
+
+exports.getProfesionalById = function (req, res){
+  models.tipoProfesional.findOne({_id:req.params.code},function (err, profesional){
+    if (err) {
+      res.send('error');
+    }else{
+      res.send({profesionales:profesional});
+    }
+  });
+}
